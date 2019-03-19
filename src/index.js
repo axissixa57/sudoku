b0ab84c69a88f88c0b801a7f13d67d6e839f5364
Ax1S <axissixa@mail.ru>
2019-03-20 01:48:42 +0300

    feat: add solution to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,90 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  function solveSudoku(board, n) {
+    let row;
+    let col;
+    let isEmpty = true;
+
+    for (let i = 0; i < n; i++) 
+    {
+        for (let j = 0; j < n; j++) 
+        {
+            if (board[i][j] == 0) 
+            {
+                row = i;
+                col = j;
+
+                isEmpty = false; // т.к. у нас есть ещЄ 0 в €чейках 
+                break;
+            }
+        }
+        if (!isEmpty)
+        {
+            break;
+        }
+    }
+
+    // 0 не осталось в €чейках 
+    if (isEmpty)
+    { 
+        return true; 
+    } 
+
+    for (let num = 1; num <= n; num++) 
+    {
+        if (isSafe(board, row, col, num)) 
+        {
+            board[row][col] = num; 
+            if (solveSudoku(board, n))  
+            {
+                return board;
+            }   
+            else
+            {
+                board[row][col] = 0; // replace it 
+            }
+        }
+    }
+    return false;
+  }
+
+  function isSafe(board, row, col, num) {
+    for (let d = 0; d < board.length; d++) 
+    {
+        if (board[row][d] == num)
+        {
+            return false;
+        }
+    }
+
+    for (let r = 0; r < board.length; r++) 
+    {
+        if (board[r][col] == num)
+        {
+            return false;
+        }
+    }
+
+    let sqrt = Math.sqrt(board.length);
+    let boxRowStart = row - row % sqrt;
+    let boxColStart = col - col % sqrt;
+
+    for (let r = boxRowStart; r < boxRowStart + sqrt; r++) 
+    {
+        for (let d = boxColStart; d < boxColStart + sqrt; d++)
+        {
+            if (board[r][d] == num) 
+            {
+                return false;
+            }
+        }
+    }
+
+    return true; // if there is no clash, it's safe 
+  }
+
+  let board = matrix; 
+  let n = board.length;
+  let result = solveSudoku(board, n);
+
+  return result;
 }
